75b07e9e5ca0e16f47888a703115007ce5eeac03
mic <mic@mix64.com>
2024-11-14 16:41:14 +0900

    Refactor hexadecimal output functions to improve readability

--- boot/common.c
+++ boot/common.c
@@ -2,30 +2,29 @@
 
 void puts(uint16 *s) { ST->ConOut->OutputString(ST->ConOut, s); }
 
-void put_hex(uint64 n) {
-    uint16 hex[] = L"0123456789ABCDEF";
-    uint16 buf[17];
-    buf[16] = 0;
-    for (int i = 0; i < 16; i++) {
-        buf[15 - i] = hex[n & 0xF];
-        n >>= 4;
+void printn(uint64 n, uint64 base) {
+    static uint16 digits[] = L"0123456789ABCDEF";
+    static uint16 buf[2] = L"0";
+    uint64 m;
+    if ((m = n / base)) {
+        printn(m, base);
     }
-    puts(L"0x");
+    buf[0] = digits[(n % base)];
     puts(buf);
 }
 
 void put_param(uint16 *s, uint64 n) {
     puts(s);
-    puts(L": ");
-    put_hex(n);
+    puts(L": 0x");
+    printn(n, 16);
     puts(L"\r\n");
 }
 
 void warn(uint64 status, uint16 *message) {
     if (status) {
         puts(message);
-        puts(L":");
-        put_hex(status);
+        puts(L": ");
+        printn(status, 16);
         puts(L"\r\n");
     }
 }
