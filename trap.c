be73dbcd7de32ef6b5c1643c482ce3a661d075c8
mic <mic@mix64.com>
2024-12-21 16:16:25 +0900

    Improve trapframe dump formatting for better readability

--- trap.c
+++ trap.c
@@ -9,26 +9,27 @@ void trap(struct trapframe *tf) {
 }
 
 void dump(struct trapframe *tf) {
-    kprintf("tf->r15 = %x\n", tf->r15);
-    kprintf("tf->r14 = %x\n", tf->r14);
-    kprintf("tf->r13 = %x\n", tf->r13);
-    kprintf("tf->r12 = %x\n", tf->r12);
-    kprintf("tf->r11 = %x\n", tf->r11);
-    kprintf("tf->r10 = %x\n", tf->r10);
-    kprintf("tf->r9 = %x\n", tf->r9);
-    kprintf("tf->r8 = %x\n", tf->r8);
-    kprintf("tf->rdi = %x\n", tf->rdi);
-    kprintf("tf->rsi = %x\n", tf->rsi);
-    kprintf("tf->rbp = %x\n", tf->rbp);
-    kprintf("tf->rbx = %x\n", tf->rbx);
-    kprintf("tf->rdx = %x\n", tf->rdx);
-    kprintf("tf->rcx = %x\n", tf->rcx);
-    kprintf("tf->rax = %x\n", tf->rax);
-    kprintf("tf->trapno = %x\n", tf->trapno);
-    kprintf("tf->err = %x\n", tf->err);
-    kprintf("tf->rip = %x\n", tf->rip);
-    kprintf("tf->cs = %x\n", tf->cs);
-    kprintf("tf->rflags = %x\n", tf->rflags);
-    kprintf("tf->rsp = %x\n", tf->rsp);
-    kprintf("tf->ss = %x\n", tf->ss);
+    kprintf("trapframe:\n");
+    kprintf("    trapno = %x\n", tf->trapno);
+    kprintf("    r15 = %x\n", tf->r15);
+    kprintf("    r14 = %x\n", tf->r14);
+    kprintf("    r13 = %x\n", tf->r13);
+    kprintf("    r12 = %x\n", tf->r12);
+    kprintf("    r11 = %x\n", tf->r11);
+    kprintf("    r10 = %x\n", tf->r10);
+    kprintf("     r9 = %x\n", tf->r9);
+    kprintf("     r8 = %x\n", tf->r8);
+    kprintf("    rdi = %x\n", tf->rdi);
+    kprintf("    rsi = %x\n", tf->rsi);
+    kprintf("    rbp = %x\n", tf->rbp);
+    kprintf("    rbx = %x\n", tf->rbx);
+    kprintf("    rdx = %x\n", tf->rdx);
+    kprintf("    rcx = %x\n", tf->rcx);
+    kprintf("    rax = %x\n", tf->rax);
+    kprintf("    err = %x\n", tf->err);
+    kprintf("    rip = %x\n", tf->rip);
+    kprintf("    rsp = %x\n", tf->rsp);
+    kprintf("    cs  = %x\n", tf->cs);
+    kprintf("    ss  = %x\n", tf->ss);
+    kprintf("    rflags = %x\n", tf->rflags);
 }
