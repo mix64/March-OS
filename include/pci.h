bd59a7e0b822de75382c4ec625f0821f1c41859a
mic <mic@mix64.com>
2025-01-16 11:08:58 +0900

    Add PCI device structures and configuration functions

--- include/pci.h
+++ include/pci.h
@@ -1,3 +1,72 @@
 #pragma once
 
+#include <types.h>
+
+enum pci_header_type {
+    PCI_HEADER_TYPE_GENERAL = 0,
+    PCI_HEADER_TYPE_BRIDGE = 1,
+    PCI_HEADER_TYPE_CARDBUS = 2,
+};
+
+enum pci_class_code {
+    PCI_CLASS_CODE_UNCLASSIFIED = 0x00,
+    PCI_CLASS_CODE_STORAGE = 0x01,
+    PCI_CLASS_CODE_NETWORK = 0x02,
+    PCI_CLASS_CODE_DISPLAY = 0x03,
+    PCI_CLASS_CODE_MULTIMEDIA = 0x04,
+    PCI_CLASS_CODE_MEMORY = 0x05,
+    PCI_CLASS_CODE_BRIDGE = 0x06,
+    PCI_CLASS_CODE_SIMPLE_COMM = 0x07,
+    PCI_CLASS_CODE_BASE_SYSTEM_PERIPHERAL = 0x08,
+    PCI_CLASS_CODE_INPUT_DEVICE = 0x09,
+    PCI_CLASS_CODE_DOCKING_STATION = 0x0A,
+    PCI_CLASS_CODE_PROCESSOR = 0x0B,
+    PCI_CLASS_CODE_SERIAL_BUS = 0x0C,
+    PCI_CLASS_CODE_WIRELESS = 0x0D,
+    PCI_CLASS_CODE_INTELLIGENT_IO = 0x0E,
+    PCI_CLASS_CODE_SATELLITE_COMM = 0x0F,
+    PCI_CLASS_CODE_ENCRYPTION = 0x10,
+    PCI_CLASS_CODE_SIGNAL = 0x11,
+    PCI_CLASS_CODE_PROCESSING_ACCELERATOR = 0x12,
+    PCI_CLASS_CODE_NON_ESSENTIAL = 0x13,
+    PCI_CLASS_CODE_COPROCESSOR = 0x40,
+    PCI_CLASS_CODE_UNASSIGNED = 0xFF,
+};
+
+struct pci_device_header {
+    uint8 bus;
+    uint8 slot;
+    uint8 func;
+
+    // Common Header Fields
+    uint16 vendor_id;
+    uint16 device_id;
+    uint16 command;
+    uint16 status;
+    uint8 rev_id;
+    uint8 prog_if;
+    uint8 subclass_id;
+    uint8 class_id;
+    uint8 cache_line_size;
+    uint8 latency_timer;
+    uint8 header_type;
+    uint8 bist;
+};
+
+struct pci_device {
+    struct pci_device_header header;
+
+    // General PCI Device Fields
+    uint32 bars[6];
+    uint32 cardbus_cis_pointer;
+    uint16 subsystem_vendor_id;
+    uint16 subsystem_id;
+    uint32 expansion_rom_base;
+    uint8 cap_pointer;
+    uint8 interrupt_line;
+    uint8 interrupt_pin;
+    uint8 min_grant;
+    uint8 max_latency;
+};
+
 void pci_scan();
--- pci.c
+++ pci.c
@@ -8,26 +8,118 @@
 
 #define PCI_CONFIG_DATA 0xCFC
 
-uint16 pci_config_read16(uint32 bus, uint32 slot, uint32 func, uint32 offset) {
-    uint32 address = PCI_CONFIG_ENABLE | (bus << 16) | (slot << 11) |
-                     (func << 8) | (offset & 0xFC);
+#define PCI_DEV_MAX 32
+static struct pci_device pci_devices[PCI_DEV_MAX];
+
+uint32 pci_config_read(uint8 bus, uint8 slot, uint8 func, uint8 offset) {
+    uint32 _bus = (uint32)bus;
+    uint32 _slot = (uint32)slot;
+    uint32 _func = (uint32)func;
+
+    uint32 address = PCI_CONFIG_ENABLE | (_bus << 16) | (_slot << 11) |
+                     (_func << 8) | (offset & 0xFC);
     outl(PCI_CONFIG_ADDRESS, address);
-    return (uint16)((inl(PCI_CONFIG_DATA) >> ((offset & 2) * 8)) & 0xFFFF);
+    return inl(PCI_CONFIG_DATA);
 }
 
-void check_device(uint16 bus, uint8 dev) {
-    uint16 vendor = pci_config_read16(bus, dev, 0, 0);
-    if (vendor == 0xFFFF) {
-        return;
-    }
-    uint16 device = pci_config_read16(bus, dev, 0, 2);
-    debugf("[PCI] Found device: %x:%x\n", vendor, device);
+uint16 pci_config_read16(uint8 bus, uint8 slot, uint8 func, uint8 offset) {
+    uint32 data = pci_config_read(bus, slot, func, offset);
+    return (uint16)((data >> ((offset & 2) * 8)) & 0xFFFF);
+}
+
+uint8 pci_config_read8(uint8 bus, uint8 slot, uint8 func, uint8 offset) {
+    uint32 data = pci_config_read(bus, slot, func, offset);
+    return (uint8)((data >> ((offset & 3) * 8)) & 0xFF);
+}
+
+void pci_read_header(uint8 bus, uint8 slot, uint8 func,
+                     struct pci_device_header *header) {
+    header->bus = bus;
+    header->slot = slot;
+    header->func = func;
+
+    header->vendor_id = pci_config_read16(bus, slot, func, 0);
+    header->device_id = pci_config_read16(bus, slot, func, 2);
+    header->command = pci_config_read16(bus, slot, func, 4);
+    header->status = pci_config_read16(bus, slot, func, 6);
+    header->rev_id = pci_config_read8(bus, slot, func, 8);
+    header->prog_if = pci_config_read8(bus, slot, func, 9);
+    header->subclass_id = pci_config_read8(bus, slot, func, 10);
+    header->class_id = pci_config_read8(bus, slot, func, 11);
+    header->cache_line_size = pci_config_read8(bus, slot, func, 12);
+    header->latency_timer = pci_config_read8(bus, slot, func, 13);
+    header->header_type = pci_config_read8(bus, slot, func, 14);
+    header->bist = pci_config_read8(bus, slot, func, 15);
+
+    kprintf("[PCI] Found Device:\n");
+    kprintf("        Vendor: %x\n", header->vendor_id);
+    kprintf("        Device: %x\n", header->device_id);
+    kprintf("        Type: %x\n", header->header_type);
+    debugf("        Status: %x\n", header->status);
+    debugf("        Command: %x\n", header->command);
+    debugf("        Class: %x\n", header->class_id);
+    debugf("        Subclass: %x\n", header->subclass_id);
+    debugf("        Prog IF: %x\n", header->prog_if);
+    debugf("        Rev ID: %x\n", header->rev_id);
+    debugf("        BIST: %x\n", header->bist);
+    debugf("        Latency Timer: %x\n", header->latency_timer);
+    debugf("        Cache Line Size: %x\n", header->cache_line_size);
+}
+
+// General PCI Device Fields
+void pci_read_table(struct pci_device *pcidev) {
+    uint8 bus = pcidev->header.bus;
+    uint8 slot = pcidev->header.slot;
+    uint8 func = pcidev->header.func;
+
+    pcidev->bars[0] = pci_config_read(bus, slot, func, 0x10);
+    pcidev->bars[1] = pci_config_read(bus, slot, func, 0x14);
+    pcidev->bars[2] = pci_config_read(bus, slot, func, 0x18);
+    pcidev->bars[3] = pci_config_read(bus, slot, func, 0x1C);
+    pcidev->bars[4] = pci_config_read(bus, slot, func, 0x20);
+    pcidev->bars[5] = pci_config_read(bus, slot, func, 0x24);
+    pcidev->cardbus_cis_pointer = pci_config_read(bus, slot, func, 0x28);
+    pcidev->subsystem_vendor_id = pci_config_read16(bus, slot, func, 0x2C);
+    pcidev->subsystem_id = pci_config_read16(bus, slot, func, 0x2E);
+    pcidev->expansion_rom_base = pci_config_read(bus, slot, func, 0x30);
+    pcidev->cap_pointer = pci_config_read8(bus, slot, func, 0x34);
+    pcidev->interrupt_line = pci_config_read8(bus, slot, func, 0x3C);
+    pcidev->interrupt_pin = pci_config_read8(bus, slot, func, 0x3D);
+    pcidev->min_grant = pci_config_read8(bus, slot, func, 0x3E);
+    pcidev->max_latency = pci_config_read8(bus, slot, func, 0x3F);
+
+    debugf("        BAR0: %x\n", pcidev->bars[0]);
+    debugf("        BAR1: %x\n", pcidev->bars[1]);
+    debugf("        BAR2: %x\n", pcidev->bars[2]);
+    debugf("        BAR3: %x\n", pcidev->bars[3]);
+    debugf("        BAR4: %x\n", pcidev->bars[4]);
+    debugf("        BAR5: %x\n", pcidev->bars[5]);
+    debugf("        Cardbus CIS Pointer: %x\n", pcidev->cardbus_cis_pointer);
+    debugf("        Subsystem Vendor ID: %x\n", pcidev->subsystem_vendor_id);
+    debugf("        Subsystem ID: %x\n", pcidev->subsystem_id);
+    debugf("        Expansion ROM Base: %x\n", pcidev->expansion_rom_base);
+    debugf("        Cap Pointer: %x\n", pcidev->cap_pointer);
+    debugf("        Interrupt Line: %x\n", pcidev->interrupt_line);
+    debugf("        Interrupt Pin: %x\n", pcidev->interrupt_pin);
+    debugf("        Min Grant: %x\n", pcidev->min_grant);
+    debugf("        Max Latency: %x\n", pcidev->max_latency);
 }
 
 void pci_scan() {
-    for (uint16 bus = 0; bus < 256; bus++) {
-        for (uint8 dev = 0; dev < 32; dev++) {
-            check_device(bus, dev);
+    int idx = 0;
+    for (uint32 bus = 0; bus < 256; bus++) {
+        for (uint32 slot = 0; slot < 32; slot++) {
+            if (pci_config_read16(bus, slot, 0, 0) == 0xFFFF) {
+                continue;
+            }
+            pci_read_header(bus, slot, 0, &pci_devices[idx].header);
+            pci_read_table(&pci_devices[idx]);
+
+            if (idx < PCI_DEV_MAX) {
+                idx++;
+            } else {
+                panic("PCI Device limit reached\n");
+            }
         }
     }
 }
