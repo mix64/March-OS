860db8eca7a694270d8913b16f8428927937be50
mic <mic@mix64.com>
2025-02-09 18:55:57 +0900

    serial: extend KPRINTF_ARG_COUNT and add support for up to 7 arguments

--- include/serial.h
+++ include/serial.h
@@ -6,8 +6,8 @@ void kprint(char *str);
 void _kprintf(char *fmt, uint8 argc, uintptr *argv);
 
 #define KPRINTF_ARG_COUNT(...) \
-    KPRINTF_ARG_COUNT_IMPL(0, ##__VA_ARGS__, 4, 3, 2, 1, 0)
-#define KPRINTF_ARG_COUNT_IMPL(_0, _1, _2, _3, _4, N, ...) N
+    KPRINTF_ARG_COUNT_IMPL(0, ##__VA_ARGS__, 7, 6, 5, 4, 3, 2, 1, 0)
+#define KPRINTF_ARG_COUNT_IMPL(_0, _1, _2, _3, _4, _5, _6, _7, N, ...) N
 
 #define KPRINTF_CAST_ARGS_0()
 #define KPRINTF_CAST_ARGS_1(a1) (uintptr)(a1)
@@ -16,6 +16,14 @@ void _kprintf(char *fmt, uint8 argc, uintptr *argv);
     (uintptr)(a1), (uintptr)(a2), (uintptr)(a3)
 #define KPRINTF_CAST_ARGS_4(a1, a2, a3, a4) \
     (uintptr)(a1), (uintptr)(a2), (uintptr)(a3), (uintptr)(a4)
+#define KPRINTF_CAST_ARGS_5(a1, a2, a3, a4, a5) \
+    (uintptr)(a1), (uintptr)(a2), (uintptr)(a3), (uintptr)(a4), (uintptr)(a5)
+#define KPRINTF_CAST_ARGS_6(a1, a2, a3, a4, a5, a6)                            \
+    (uintptr)(a1), (uintptr)(a2), (uintptr)(a3), (uintptr)(a4), (uintptr)(a5), \
+        (uintptr)(a6)
+#define KPRINTF_CAST_ARGS_7(a1, a2, a3, a4, a5, a6, a7)                        \
+    (uintptr)(a1), (uintptr)(a2), (uintptr)(a3), (uintptr)(a4), (uintptr)(a5), \
+        (uintptr)(a6), (uintptr)(a7)
 
 #define KPRINTF_CAT(a, b) KPRINTF_CAT_IMPL(a, b)
 #define KPRINTF_CAT_IMPL(a, b) a##b
