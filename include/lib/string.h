57b63223d8736e2f881463a8fd8a40486eeeaa1a
mic <mic@mix64.com>
2025-03-03 15:59:13 +0900

    lib/string: impl memcmp() and strcmp()

--- include/lib/string.h
+++ include/lib/string.h
@@ -3,4 +3,5 @@
 
 void memset(void *addr, uint8 data, uint64 count);
 void memcpy(void *dst, void *src, uint64 count);
-int memcmp(const uint8 *p, const uint8 *q, uint64 limit);
+int memcmp(const void *p, const void *q, uint64 limit);
+int strcmp(const char *dest, const char *src);
--- string.c
+++ string.c
@@ -20,3 +20,26 @@ void memcpy(void *dest, void *src, uint64 count) {
     }
 #endif
 }
+
+int memcmp(const void *p, const void *q, uint64 limit) {
+    for (uint64 i = 0; i < limit; i++) {
+        if (((uint8 *)p)[i] != ((uint8 *)q)[i]) {
+            return 1;
+        }
+    }
+    return 0;
+}
+
+int strcmp(const char *dest, const char *src) {
+    while (*dest != '\0' && *src != '\0') {
+        if (*dest != *src) {
+            return 1;
+        }
+        dest++;
+        src++;
+    }
+    if (*dest != *src) {
+        return 1;
+    }
+    return 0;
+}
