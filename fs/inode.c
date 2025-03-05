684edf5ab9d6a492f8d09f3220ab76b630589049
mic <mic@mix64.com>
2025-03-05 17:47:18 +0900

    fs: add inode management with ialloc() and ifree()

---
+++ fs/inode.c
@@ -0,0 +1,32 @@
+#include <fs/inode.h>
+#include <list.h>
+#include <mm.h>
+
+static list_t inode_list;
+
+inode_t *ialloc(uint64 id) {
+    // find inode by id
+    for (list_t *p = &inode_list; p != NULL; p = p->next) {
+        inode_t *ip = (inode_t *)p->node;
+        if (ip->id == id) {
+            ip->refcnt++;
+            return ip;
+        }
+    }
+
+    // allocate new inode
+    inode_t *ip = (inode_t *)kmalloc(sizeof(inode_t));
+    ip->id = id;
+    ip->size = 0;
+    ip->refcnt = 1;
+    list_push(&inode_list, ip);
+    return ip;
+}
+
+void ifree(inode_t *ip) {
+    if (ip->refcnt > 1) {
+        ip->refcnt--;
+    }
+    list_remove(&inode_list, ip);
+    kmfree(ip);
+}
