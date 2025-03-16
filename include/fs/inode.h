3755a5efed42420f22f51f0451c515c966af5aa8
mic <mic@mix64.com>
2025-03-16 23:38:12 +0900

    fs: add inode_type enum and type field to inode structure

--- include/fs/inode.h
+++ include/fs/inode.h
@@ -2,11 +2,19 @@
 
 #include <types.h>
 
+enum inode_type {
+    INODE_T_FILE,
+    INODE_T_DIR,
+    INODE_T_DEV,
+};
+
 typedef struct inode {
     uint64 id;    // inode number
     uint64 size;  // file size
     uint16 refcnt;
+    enum inode_type type;  // file type
 } inode_t;
 
+void iinit();
 inode_t *ialloc(uint64 id);
 void ifree(inode_t *ip);
