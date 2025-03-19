720788631d5fdb7019d6439fa54a1dae3ac6860a
mic <mic@mix64.com>
2025-03-19 13:49:27 +0900

    fs: remove inode_type enum and type field from inode structure

--- include/fs/inode.h
+++ include/fs/inode.h
@@ -2,17 +2,10 @@
 
 #include <types.h>
 
-enum inode_type {
-    INODE_T_FILE,
-    INODE_T_DIR,
-    INODE_T_DEV,
-};
-
 typedef struct inode {
     uint64 id;    // inode number
     uint64 size;  // file size
     uint16 refcnt;
-    enum inode_type type;  // file type
 } inode_t;
 
 void iinit();
